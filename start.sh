94a702e2954954b83f06f6f2eff3c9f09ec07679
Candid Dauth <cdauth@cdauth.eu>
2016-02-13 19:13:25 +0100

    Run renew-ssl.sh automatically

--- start.sh
+++ start.sh
@@ -4,4 +4,13 @@ mkdir -p /ssl/letsencrypt && chown acme:acme /ssl/letsencrypt
 
 /usr/local/bin/mkconfig.sh > /usr/local/apache2/conf/extra/vhosts.conf
 
+su acme -c /usr/local/bin/renew-ssl.sh
+
+(
+	while true; do
+		sleep 86400
+		su acme -c /usr/local/bin/renew-ssl.sh
+	done
+) &
+
 /usr/local/bin/httpd-foreground
