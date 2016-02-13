b7bc1ac8d6fb685c5d8092cb2dbb3778d8927bb0
Candid Dauth <cdauth@cdauth.eu>
2016-02-13 19:12:24 +0100

    Code to check cert date and san is not necessary, as it is done by simp_le

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -13,38 +13,6 @@ for i in ${!HOST_*}; do
 	hosts+=("$(echo "${i#HOST_}" | sed -e s/_/./g)")
 done
 
-if [ ! -e cert.pem ]; then
-	echo "Certificate does not exist, creating."
-else
-
-	date_cert="$(date --date="$(openssl x509 -startdate -in cert.pem -noout | cut -d= -f2)" +%s)"
-	date_now="$(date +%s)"
-	days_old="$[($date_now-$date_old)/86400]"
-
-	if [ "$days_old" -gt 30 ]; then
-		echo "Certificate $days_old days old, recreating."
-	else
-		IFSb="$IFS"
-		IFS=", "
-		san=$(openssl x509 -text -in cert.pem -noout | grep -A1 'X509v3 Subject Alternative Name:' | tail -1)
-		IFS="$IFSb"
-
-		missing_hosts=()
-		for i in "${hosts[@]}"; do
-			if ! containsElement "DNS:$i" "${san[@]}"; then
-				missing_hosts+=("$i")
-			fi
-		done
-
-		if [ "${#missing_hosts[@]}" -gt 0 ]; then
-			echo "Missing SAN hosts ${missing_hosts[@]}. Recreating certificate."
-		else
-			echo "Certificate is $days_old days old. Not recreating."
-			exit 0
-		fi
-	fi
-fi
-
 args=(-f account_key.json -f cert.pem -f chain.pem -f key.pem --default_root /usr/local/apache2/htdocs)
 
 for i in "${hosts[@]}"; do
