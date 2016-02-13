6b9e3ea95f93b72a4d1777f3074144e316a4267d
Candid Dauth <cdauth@cdauth.eu>
2016-02-13 19:03:03 +0100

    Add renew-ssl.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /ssl/letsencrypt && chown acme:acme /ssl/letsencrypt
+
 /usr/local/bin/mkconfig.sh > /usr/local/apache2/conf/extra/vhosts.conf
 
 /usr/local/bin/httpd-foreground
